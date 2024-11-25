02fa17c648470d953a8a560c7df1b860b84d0018
Rafael Diniz <rafael@riseup.net>
2024-11-25 13:58:36 +0000

    add examples directory

---
+++ examples/clib.h
@@ -0,0 +1,8 @@
+#ifndef CLIB_H_
+#define CLIB_H_
+
+int write(int fd, void *buffer, int size);
+int exit(int code);
+
+
+#endif // CLIB_H_
---
+++ examples/test.c
@@ -0,0 +1,8 @@
+
+#include "clib.h"
+
+int main()
+{
+	write(1, "Hello World", 11);
+	exit(0);
+}
