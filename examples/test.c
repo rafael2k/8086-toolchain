9fb04c44e08a47544ca83580abdcfd88b299332c
Rafael Diniz <rafael@riseup.net>
2024-12-06 18:17:30 +0000

    add a test that crashes C86

--- examples/test.c
+++ examples/test.c
@@ -1,8 +1,13 @@
+#include "nanoprintf.h"
+
 extern int write(int, char *, int);
 extern void exit(int);
 
 int main()
 {
-	write(1, "Hello World\n", 12);
+	char buffer[128];
+	// write(1, "Hello World\n", 12);
+	npf_snprintf(buffer, 128, "Hello %s!\n", "World");
+	write(1, buffer, 13);
 	exit(0);
 }
